edac93a3130d6d2ab21c6cc1912e0f5af8615ff2
Chethan Agarwal <chethanagarwal@Chethans-MacBook-Pro.local>
2024-12-14 18:52:04 -0500

    menu changes

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-import Navbar from './components/Navbar';
-import Header from './components/Header';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import "./Menu.css"
 
 const Menu = () => {
-    return (
-        <>
-        <Header></Header>
-        <Navbar></Navbar>
-        </>
-      );
-}
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <div id="menutop">
+        <div style={{ flex: "1" }}>
+          <h2>Soup of the Day</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia odio vitae vestibulum vestibulum.
+          </p>
+        </div>
+        <div id="bites">
+          <h2>Small Bites</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia odio vitae vestibulum vestibulum.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Menu;
