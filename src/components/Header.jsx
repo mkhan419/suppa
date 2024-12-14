46e70aac04571072e0d378c1bd4231dba8ff9bc7
Misha Khan <mishak@mishas-air.umd.edu>
2024-12-13 19:49:54 -0500

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+export default function Header() {
+    return (
+    <div id="box">
+        <div id="name">
+            <h3>"soup-uh"</h3>
+            <h2 id="suppa">suppa</h2>
+            <h3>soup bar</h3>
+       </div>
+    </div>
+    );
+}
