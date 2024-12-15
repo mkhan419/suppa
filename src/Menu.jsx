0bb8618152c155b71c971aa211ac3d58e6896822
Chethan Agarwal <chethanagarwal@Chethans-MacBook-Pro.local>
2024-12-14 19:51:20 -0500

    menu stuff

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,23 +1,79 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import "./Menu.css"
+import "./Menu.css";
 
 const Menu = () => {
   return (
     <>
       <Header />
       <Navbar />
+
       <div id="menutop">
         <div style={{ flex: "1" }}>
-          <h2>Soup of the Day</h2>
-          <p>
+          <h1>Soup of the Day</h1>
+          <p id="menus">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
             lacinia odio vitae vestibulum vestibulum.
           </p>
         </div>
         <div id="bites">
-          <h2>Small Bites</h2>
+          <h1>Small Bites</h1>
+          <p id="menus">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia odio vitae vestibulum vestibulum.
+          </p>
+        </div>
+      </div>
+      <h1 id="worldly">Soup Around the World</h1>
+      <div id="world" style={{ display: "flex" }}>
+        <div id="latin">
+          <h2>Latin America</h2>
+
+
+          <h4>Chicken Tortilla Soup</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia odio vitae vestibulum vestibulum.
+          </p>
+
+          <h4>Moqueca</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia odio vitae vestibulum vestibulum.
+          </p>
+
+          <h4>Vori</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia odio vitae vestibulum vestibulum.
+          </p>
+
+          <h4>Tacaca</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia odio vitae vestibulum vestibulum.
+          </p>
+        </div>
+        <div id="asian">
+          <h2>Asian</h2>
+          <br />
+          <h4>Ramen</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia odio vitae vestibulum vestibulum.
+          </p>
+          <h4>Pho</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia odio vitae vestibulum vestibulum.
+          </p>
+          <h4>Miso Soup</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            lacinia odio vitae vestibulum vestibulum.
+          </p>
+          <h4>Spicy Wonton</h4>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
             lacinia odio vitae vestibulum vestibulum.
